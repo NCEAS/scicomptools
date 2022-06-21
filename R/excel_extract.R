810460ca4fb5ed7bd0b1e8aad098c154a350fae8
njlyon0 <nicholasjlyon@gmail.com>
2022-06-21 11:21:43 -0500

    Finished first version of Excel format checker function. Also renamed the reading excel sheets function

--- R/excel_extract.R
+++ R/excel_extract.R
@@ -1,27 +1,74 @@
 #' @title Read All Sheets from an Excel Workbook
 #'
-#' @description Retrieves all of the sheets in a given Microsfot Excel workbook and stores them as elements in a list. Note that the guts of this function were created by the developers of `readxl` and a tutorial-ized version of this function can be found here: https://readxl.tidyverse.org/articles/readxl-workflows.html (Note however that they didn't have it as a function)
+#' @description Retrieves all of the sheets in a given Microsoft Excel workbook and stores them as elements in a list. Note that the guts of this function were created by the developers of `readxl::read_excel()` and we merely created a wrapper function to invoke their work more easily.
 #'
-#' @param filename (character) Name of the Excel workbook
+#' @param file_name (character) Name of (and path to) the Excel workbook
+#'
+#' @return (list) One tibble per sheet in the Excel workbook stored as separate elements in a list
 #'
 #' @export
 #'
-read_full_excel <- function(filename = NULL) {
-  # Error out if no filename is provided
-  if(base::is.null(filename)) stop("No file provided")
+read_xl_sheets <- function(file_name = NULL) {
+  # Error out if no file name is provided
+  if(base::is.null(file_name)) stop("No file provided")
 
   # For a given Excel file
-  excel_data <- filename %>%
+  excel_data <- file_name %>%
     # Retrieve the names of all of the sheets
     readxl::excel_sheets() %>%
     # Name each list element by the corresponding sheet names
     purrr::set_names() %>%
     # For each name, read in the sheet to that list element
-    purrr::map(readxl::read_excel, path = filename)
+    purrr::map(readxl::read_excel, path = file_name)
 
   # Return that list
   return(excel_data)
 }
+
+#' @title Read Formatting of All Sheets in an Excel Workbook
+#'
+#' @description Retrieves all sheets of a Microsoft Excel workbook and identifies the formatting of each value (including column headers and blank cells).
+#'
+#' @param file_name (character) Name of (and path to) the Excel workbook
+#'
+#' @return (data frame) One row per cell in the dataframe with a column for each type of relevant formatting and its 'address' within the original Excel workbook
+#'
+#' @export
+#'
+read_xl_format <- function(file_name = NULL){
+
+  # Error out if no file name is provided
+  if(base::is.null(file_name)) stop("No file provided")
+
+# Otherwise, identify contents and format of all sheets
+contents <- tidyxl::xlsx_cells(path = file_name)
+formats <- tidyxl::xlsx_formats(path = file_name)
+
+# Now let's pare down the contents dataframe
+output <- contents %>%
+  dplyr::mutate(
+    # Coerce non-character cells into characters
+    error_char = as.character(error),
+    logical_char = as.character(logical),
+    numeric_char = as.character(numeric),
+    date_char = as.character(date),
+    # So that we can `coalesce()` them into a single column
+    cell_contents = dplyr::coalesce(error_char, logical_char, numeric_char, date_char, character),
+    # Now retrieve necessary formatting information
+    bold = formats$local$font$bold[.$local_format_id],
+    italic = formats$local$font$italic[.$local_format_id],
+    underline = formats$local$font$underline[.$local_format_id],
+    font_size = formats$local$font$size[.$local_format_id],
+    font_color = formats$local$font$color$rgb[.$local_format_id],
+    cell_color = formats$local$fill$patternFill$bgColor$rgb[.$local_format_id]
+  ) %>%
+  dplyr::select(sheet, address, row, col, cell_contents, comment, bold, italic, underline, font_size, font_color, cell_color)
+
+# And return that output dataframe
+return(output)
+
+}
+
 
 #' Export csv files out of an Excel workbook including formatting and formulas
 #'
@@ -61,10 +108,10 @@ output_to_csv <- function(filename) {
 #'
 #' @export
 #'
-output_sheet <- function(filename, directory, sheet, n) {
-  x <-  tidyxl::xlsx_cells(filename)
+output_sheet <- function(filename, directory = getwd(), sheet, n) {
+  x <-  tidyxl::xlsx_cells(path = filename)
 
-  y <-  tidyxl::xlsx_formats(filename)
+  y <-  tidyxl::xlsx_formats(path = filename)
 
   output_value(x, y, directory, sheet)
   output_bold(x, y, directory, sheet)
@@ -121,7 +168,10 @@ output_value <- function(contents, formats, directory, sheet) {
 #'
 output_bold <- function(contents, formats, directory, sheet) {
   bolds <- contents[[sheet]]$local_format_id %in% which(formats$local$font$bold)
-  output <- matrix(data=bolds, nrow=max(contents[[sheet]][["row"]]),ncol=max(contents[[sheet]][["col"]]), byrow = TRUE)
+  output <- matrix(data = bolds,
+                   nrow = max(contents[[sheet]][["row"]]),
+                   ncol = max(contents[[sheet]][["col"]]),
+                   byrow = TRUE)
   output <- data.frame(output)
   output_name <- sprintf("%s%s_bold.csv", directory, sheet)
   utils::write.table(output,file=output_name, row.names=FALSE,col.names=FALSE, sep = ",")
