c0af6f4a4038a830346954ff7caa37724ddba8cc
njlyon0 <nicholasjlyon@gmail.com>
2022-06-24 12:16:19 -0500

    Added support for checking GitHub Personal Access Token with `token_check()`

--- R/token_check.R
+++ R/token_check.R
@@ -2,7 +2,7 @@
 #'
 #' @description To make some direct-from-API workflows functional (e.g., Qualtrics surveys, etc.). It is necessary to quickly test whether a given R session "knows" the API token. This function returns an error if the specified token type isn't found and prints a message if one is found
 #'
-#' @param api (character) API the token is for (currently only supports "qualtrics")
+#' @param api (character) API the token is for (currently only supports "qualtrics" and "github")
 #' @param secret (logical) Whether to include the token character string in the success message. FALSE prints the token, TRUE keeps it secret but returns a success message
 #'
 #' @export
@@ -10,12 +10,15 @@
 token_check <- function(api = "qualtrics", secret = TRUE){
 
   # Error out for unsupported `api`
-  if(!base::tolower(api) %in% c("qualtrics"))
-    stop("Token API not recognized. Must be 'qualtrics'")
+  if(!base::tolower(api) %in% c("qualtrics", "github"))
+    stop("Token API not recognized. Must be 'qualtrics' or 'github'")
 
   # Grab the specified token
   if(base::tolower(api) == "qualtrics"){
-    token_string = base::Sys.getenv("QUALTRICS_API_KEY")
+    token_string = base::Sys.getenv("QUALTRICS_API_KEY") }
+
+  if(base::tolower(api) == "github"){
+   token_string = gitcreds::gitcreds_get()[["password"]]
   }
 
   # Print message if no token found
