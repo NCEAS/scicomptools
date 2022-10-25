5688cf186855c0de1d62720f4d325ce791c9b7fd
njlyon0 <nicholasjlyon@gmail.com>
2022-10-25 10:38:50 -0400

    First pass at creating a function that lets users toggle back and forth between local and remote filepaths

---
+++ R/wd_loc.R
@@ -0,0 +1,30 @@
+#' @title Define Local or Remote Working Directories
+#' 
+#' @description While working on the same script both in a remote server and locally on your home computer, defining file paths can be unwieldy and may even require duplicate scripts--one for each location--that require maintainenance in parallel. This function allows you to define whether you are working locally or not and specify the path to use in either case.
+#' 
+#' @param local (logical) Whether you are working locally or on a remote server
+#' @param local_path (character) File path to use if `local` is `TRUE` (defaults to `getwd()`)
+#' @param remote_path (character) File path to use if `local` is `FALSE`
+#' 
+#' @return (character) Either the entry of `local_path` or `remote_path` depending on whether `local` is set as true or false
+#' 
+#' @export
+#' 
+wd_loc <- function(local = TRUE, local_path = getwd(), remote_path = NULL){
+  
+  # If local isn't logical, coerce to TRUE
+  if(!is.logical(local)){
+    message("`local` must be either TRUE or FALSE. Coercing to TRUE")
+    local <- TRUE}
+  
+  # If local is FALSE but remote_path isn't specified, error out
+  if(local == FALSE & is.null(remote_path))
+    stop("If working on a remote server, `remote_path` must be specified")
+  
+  # Pick user-specified path
+  if(local == TRUE){ path <- local_path }
+  if(local == FALSE){ path <- remote_path }
+  
+  # Return chosen path
+  return(path) 
+}
