aa52f74b05a045a1814ab67045440b217dd9213b
njlyon0 <nicholasjlyon@gmail.com>
2024-05-02 11:33:38 -0400

    Skeleton of testing script for each script in R folder

---
+++ tests/testthat/test-excel_extract.R
@@ -0,0 +1,8 @@
+# Run all tests in this script:
+## testthat::test_file(file.path("tests", "testthat", "test-excel_extract.R"))
+
+test_that("multiplication works", {
+  expect_equal(2 * 2, 4)
+})
+
+
---
+++ tests/testthat/test-stat_extract.R
@@ -0,0 +1,8 @@
+# Run all tests in this script:
+## testthat::test_file(file.path("tests", "testthat", "test-stat_extract.R"))
+
+test_that("multiplication works", {
+  expect_equal(2 * 2, 4)
+})
+
+
