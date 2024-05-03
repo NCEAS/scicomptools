fce7b9100f47c6e0efe04bbefa7d2c7556e80b56
njlyon0 <nicholasjlyon@gmail.com>
2024-05-03 11:45:02 -0400

    Commenting out placeholder 'multiplication works' tests

--- tests/testthat/test-molec_wt.R
+++ tests/testthat/test-molec_wt.R
@@ -1,8 +1,8 @@
 # Run all tests in this script:
 ## testthat::test_file(file.path("tests", "testthat", "test-molec_wt.R"))
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
-})
-
+# test_that("multiplication works", {
+#   expect_equal(2 * 2, 4)
+# })
+# 
 
--- tests/testthat/test-token_check.R
+++ tests/testthat/test-token_check.R
@@ -1,7 +1,7 @@
 # Run all tests in this script:
 ## testthat::test_file(file.path("tests", "testthat", "test-token_check.R"))
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
-})
+# test_that("multiplication works", {
+#   expect_equal(2 * 2, 4)
+# })
 
