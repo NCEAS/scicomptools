fc56a10a8549cc6044ab2c1a7a500883103a6257
angelchen7 <angelchen7777@yahoo.com>
2024-05-29 12:39:44 -0700

    edited unit tests for `word_cloud_prep()`

--- tests/testthat/test-word_clouds.R
+++ tests/testthat/test-word_clouds.R
@@ -4,22 +4,42 @@
 # Error testing
 test_that("Only accepts correct inputs", {
   expect_error(word_cloud_prep())
+  
+  bird_text <- data.frame(num = 1:10,
+                          birds = c("pigeon",
+                                    "finch",
+                                    "duck",
+                                    "swan",
+                                    "pigeon",
+                                    "seagull",
+                                    "cardinal",
+                                    "bluejay",
+                                    "finch",
+                                    "duck"))
+  expect_error(word_cloud_prep(data = bird_text, text_column = "seen_birds"))
+  
+  bird_text_list <- as.list(bird_text)
+  expect_error(word_cloud_prep(data = bird_text_list, text_column = "birds"))
+  
+  logical_text <- data.frame(num = 1:3,
+                             value = c(TRUE, TRUE, TRUE))
+  expect_error(word_cloud_prep(data = logical_text, text_column = "value"))
 })
 
 # Output testing
 test_that("Outputs are correct", {
-  text <- data.frame(num = 1:10,
-                     birds = c("pigeon",
-                               "finch",
-                               "duck",
-                               "swan",
-                               "pigeon",
-                               "seagull",
-                               "cardinal",
-                               "bluejay",
-                               "finch",
-                               "duck"))
-
-  expect_equal(nrow(word_cloud_prep(data = text, text_column = "birds")), 7)
-  expect_contains(class(word_cloud_prep(data = text, text_column = "birds")), "data.frame")
+  bird_text <- data.frame(num = 1:10,
+                          birds = c("pigeon",
+                                    "finch",
+                                    "duck",
+                                    "swan",
+                                    "pigeon",
+                                    "seagull",
+                                    "cardinal",
+                                    "bluejay",
+                                    "finch",
+                                    "duck"))
+  
+  expect_equal(nrow(word_cloud_prep(data = bird_text, text_column = "birds")), 7)
+  expect_contains(class(word_cloud_prep(data = bird_text, text_column = "birds")), "data.frame")
 })
